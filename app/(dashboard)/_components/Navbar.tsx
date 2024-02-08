0afa5d1c6471d764c9f735cd5dc1eb713788f691
AyodejiOmole <omoledaniel13@gmail.com>
2024-02-08 14:30:46 +0100

    added UserButton to navbar

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,9 +1,11 @@
 import MobileSidebar from "./MobileSidebar";
+import { NavbarRoutes } from "@/components/NavbarRoutes";
 
 const Navbar = () => {
     return ( 
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
             <MobileSidebar/>
+            <NavbarRoutes/>
         </div>
      );
 }
